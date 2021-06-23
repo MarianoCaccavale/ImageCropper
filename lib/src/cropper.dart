b698ce396d0adeeaf6673a369a982feb3e35a8e1
MarianoCaccavale <75137536+MarianoCaccavale@users.noreply.github.com>
2021-06-23 17:28:55 +0200

    Update cropper.dart

--- lib/src/cropper.dart
+++ lib/src/cropper.dart
@@ -112,15 +112,15 @@ class ImageCropper {
     splitResult[4] = splitResult[4].replaceAll('.', ',');
     splitResult[5] = splitResult[5].replaceAll(',', '.');
     double tmpAngle = double.parse(splitResult[5]);
-    //int tmpAngleCorr = tmpAngle.toInt();
+    int tmpAngleCorr = tmpAngle.toInt();
     
 
     return CropInfo(
       path: splitResult[0],
-      x: double.parse(splitResult[1]),
-      y: double.parse(splitResult[2]),
-      width: double.parse(splitResult[3]),
-      height: double.parse(splitResult[4]),
+      x: int.parse(splitResult[1]),
+      y: int.parse(splitResult[2]),
+      width: int.parse(splitResult[3]),
+      height: int.parse(splitResult[4]),
       angle: tmpAngle,
     );
   }
@@ -128,8 +128,8 @@ class ImageCropper {
 
 class CropInfo {
   final String path;
-  final double x, y, width, height;
-  final double angle;
+  final int x, y, width, height;
+  final int angle;
   
   get minX => x;
   get minY => y;
